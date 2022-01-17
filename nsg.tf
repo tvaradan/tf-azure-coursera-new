e43e2377ae5523a18f00b8d84768a8fd97694e5c
Varadhan Anandanpillai <varadantca@hotmail.com>
2022-01-17 02:11:20 +0000

    upload vnetsubnet

---
+++ nsg.tf
@@ -0,0 +1,21 @@
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = var.nsgname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.tags
+  }
+}
