5497bd8a5efa7f20a63fd5a5dad7f5f3043fbfa6
Varadhan Anandanpillai <varadantca@hotmail.com>
2022-01-17 02:57:57 +0000

    upload vnetsubnet

---
+++ storage-account.tf
@@ -0,0 +1,17 @@
+resource "random_id" "randomid" {
+    keepers = {
+        resource_group = azurerm_resource_group.myTerraformGroup.name
+    }
+    byte_length = 8
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "diag${random_id.randomid.hex}"
+  resource_group_name      = azurerm_resource_group.myTerraformGroup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
